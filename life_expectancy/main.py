d320dc47e2e1d684eb3c692c15584d959e7f5d09
André Bartolomeu Dias <andrebdias@MacBook-Air-2.home>
2023-05-27 01:26:48 +0100

    Small Changes assign_1

--- life_expectancy/main.py
+++ life_expectancy/main.py
@@ -1,12 +1,12 @@
 from life_expectancy.cleaning import clean_data
 import argparse
 
-def main(region):
+def main(region: str = 'PT'):
     clean_data(region)
 
 if __name__ == "__main__": # pragma: no cover
     parser = argparse.ArgumentParser()
-    parser.add_argument("--region", default="PT", help="Region code (default: PT)")
+    parser.add_argument("--region", default = 'PT', help = "Provide a Region code (default = PT)")
     args = parser.parse_args()
 
     main(args.region)
