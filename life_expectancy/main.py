03f4b8810a56be61c39aac8f053b9336db7100ed
André Bartolomeu Dias <andrebdias@MacBook-Air-2.home>
2023-05-29 18:44:04 +0100

    Fix Merge Conflits

--- life_expectancy/main.py
+++ life_expectancy/main.py
@@ -6,8 +6,12 @@ from life_expectancy.cleaning import load_data, clean_data, save_data
 
 def main(region: "PT"):
     """Main Function to Run the Pipeline
-
-
+        Args:
+        region (_type_): _description_
+    """
+    prepared_df = load_data()
+    cleaned_df = clean_data(prepared_df, region)
+    save_data(cleaned_df)
 
 if __name__ == "__main__": # pragma: no cover
     parser = argparse.ArgumentParser()
